dd685a1fc0dcf6980a0a7fe4abecaa3a635701e1
Barbara Bellassai <b.bellassai88@gmail.com>
2021-02-26 16:37:40 +0100

    Second Commit - Button Added

--- my-first-app/src/components/Body.js
+++ my-first-app/src/components/Body.js
@@ -3,16 +3,41 @@ import Proptypse from "prop-types"
 import React, { Component } from 'react'
 
 export default class Body extends Component {
+    constructor (props){
+        super(props);
+
+        this.state ={
+            showProduct: false,
+        }
+    }
+
+    makeProductsVisible(){
+        this.setState({showProduct: false,})
+    }
+
     render() {
-        const {cover,title,description} = this.props;
+        const {cover,title,description, products} = this.props;
+        const {showProducts} = this.state
         return (
             <div>
               <img src = {cover} alt = "body cover"/>
               <h3>{title}</h3> 
               <p>{description}</p>
+            
+              <div className="products-container">
+                {showProducts ? (
+                products.map((product) => {
+                return <li>{product.title}</li>;
+                     })
+                ) : (
+              <button onClick={() => this.makeProductsVisible()}>
+                Show products
+              </button>
+                )}
+                </div> 
             </div>
-        )
-    }
+        )}
+        
 }
 
 Body.proptypse ={
